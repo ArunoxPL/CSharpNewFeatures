cbfb2b395d3f54ff11afcc4c89b1e9c961bac637
Grzegorz Małek <gmalek@grupainteger.pl>
2021-02-18 14:30:46 +0100

    Rational Pattern Matching

--- TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
+++ TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
@@ -1,12 +1,27 @@
 ﻿using ConsoleUI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-PersonModel p = new () { Id = 1, FirstName = "Grzesiek", LastName = "Malek" };
-PersonModel p2 = new(2, "Paweł", "Kropka");
+var temp = new List<int> {1,3,4,6};
+string result;
 
-
-//p.Id = 2;
-//p.FirstName = "Grzegorz";
-
-Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
+foreach (var item in temp)
+{
+    //switch (item)
+    //{
+    //    case 1:
+    //        Console.WriteLine("Jeden");
+    //        break;
+    //    default:
+    //        break;
+    //}
+    result = item switch
+    {
+        < 2  => "mniej niż dwa",
+        >= 3 => "więcej niż dwa",
+        _    => "nieznane"
+    };
+    Console.WriteLine(result);
+}
 
---
+++ TopLevelCalls/NewCSharpFeatures/ConsoleUI/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public double TemperatureC { get; set; }
+        public WeatherForecast()
+        {
+        }
+    }
+}
