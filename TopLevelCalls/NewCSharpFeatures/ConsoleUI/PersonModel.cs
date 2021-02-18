b3aa043e8cf16637e3ebcb6175591ea837ca8718
Grzegorz Małek <gmalek@grupainteger.pl>
2021-02-18 13:36:16 +0100

    InitSetters

---
+++ TopLevelCalls/NewCSharpFeatures/ConsoleUI/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class PersonModel
+    {
+        public int Id { get; init; } // zmiana albo w konstruktorze albo podczas pierwszego wywołania 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
+++ TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
@@ -1,7 +1,16 @@
-﻿using System;
+﻿using ConsoleUI;
+using System;
 
 Console.WriteLine("Hello World!");
 Console.WriteLine(Add(4,3));
+
+
+PersonModel p = new PersonModel { Id = 1, FirstName = "Grzesiek", LastName = "Malek" };
+
+//p.Id = 2;
+//p.FirstName = "Grzegorz";
+
+Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
 
 static double Add(double x, double y)
 {
