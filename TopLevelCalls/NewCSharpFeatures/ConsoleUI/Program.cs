9f414f2915b9509ace229741ac456c5697029778
Grzegorz Małek <gmalek@grupainteger.pl>
2021-02-18 13:37:38 +0100

    InitSetters Cleanup

--- TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
+++ TopLevelCalls/NewCSharpFeatures/ConsoleUI/Program.cs
@@ -1,10 +1,6 @@
 ﻿using ConsoleUI;
 using System;
 
-Console.WriteLine("Hello World!");
-Console.WriteLine(Add(4,3));
-
-
 PersonModel p = new PersonModel { Id = 1, FirstName = "Grzesiek", LastName = "Malek" };
 
 //p.Id = 2;
@@ -12,7 +8,3 @@ PersonModel p = new PersonModel { Id = 1, FirstName = "Grzesiek", LastName = "Ma
 
 Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
 
-static double Add(double x, double y)
-{
-    return x + y;
-}
